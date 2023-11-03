a181b5bdaed7b346b934c9becfde3a288c245d13
Cyprien Borée <cyprien.boree@tuta.io>
2023-11-03 17:27:07 +0900

    fix: main gh url

--- fetcher/fetcher.go
+++ fetcher/fetcher.go
@@ -2,7 +2,6 @@ package fetcher
 
 import (
 	"fmt"
-	"log"
 
 	"github.com/gocolly/colly"
 )
@@ -16,34 +15,27 @@ func DownloadRepositories(username string) error {
 	if err != nil {
 		return err
 	}
-	for _, repo := range repositories {
-		fmt.Printf("url found: %v\n", repo.url)
-	}
 	fmt.Printf("# repo found: %v\n", len(repositories))
 	return nil
 }
 
 func fetchRepositories(username string) ([]Repository, error) {
-	url := "https://github.com/" + username
+	url := "https://github.com/" + username + "?tab=repositories"
 	fmt.Printf("fetching from %v\n", url)
 
 	collector := colly.NewCollector()
 
-	collector.OnError(func(_ *colly.Response, err error) {
-		log.Println("Something went wrong: ", err)
-	})
-
-	collector.OnResponse(func(r *colly.Response) {
-		fmt.Println("Page visited: ", r.Request.URL)
-	})
-
 	var repos []Repository
-	collector.OnHTML("a", func(e *colly.HTMLElement) {
-		repo := Repository{}
-		repo.url = e.Text
+	collector.OnHTML("h3 a", func(e *colly.HTMLElement) {
+		repo := Repository{
+			url: e.Attr("href"),
+		}
 		repos = append(repos, repo)
 	})
 
 	err := collector.Visit(url)
-	return repos, err
+	if err != nil {
+		return nil, err
+	}
+	return repos, nil
 }
