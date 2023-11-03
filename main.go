cbe6ca13e6d951717e91f8a24f0dff9948d9b3a0
Cyprien Borée <cyprien.boree@tuta.io>
2023-11-03 19:35:59 +0900

    feat: add --debug flag for showing debug messages

--- main.go
+++ main.go
@@ -3,22 +3,38 @@ package main
 import (
 	"flag"
 	"fmt"
+	"log/slog"
 	"os"
 
 	"github.com/boreec/repo-downloader/fetcher"
 )
 
 func main() {
+	debug := flag.Bool("debug", false, "Show debugging messages")
+
 	flag.Parse()
+
 	if flag.NArg() < 1 {
 		fmt.Println("You must provide at least one string argument!")
 		flag.PrintDefaults()
-		os.Exit(1)
+		return
+	}
+
+	if *debug {
+		setLoggerLevelToDebug()
 	}
 
 	err := fetcher.DownloadRepositories(flag.Arg(0))
 	if err != nil {
 		fmt.Println(err.Error())
-		os.Exit(1)
+		return
 	}
+}
+
+func setLoggerLevelToDebug() {
+	textHandler := slog.NewTextHandler(os.Stdout, &slog.HandlerOptions{
+		Level: slog.LevelDebug,
+	})
+	logger := slog.New(textHandler)
+	slog.SetDefault(logger)
 }
